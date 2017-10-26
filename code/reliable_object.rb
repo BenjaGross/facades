f56add94282ac283939d5ae957bed8ec036eee09
Benjamin Gross <benja.j.gross@gmail.com>
2017-10-26 12:12:07 -0400

    fix object;

--- code/reliable_object.rb
+++ code/reliable_object.rb
@@ -1,6 +1,5 @@
 class ReliableAgent
   def self.build_reliable_agents(ids)
-    Agent.all(ids).collect { |agent| agent.nil? ? NullAgent.new : agent }
-    agents.collect {|agent| agent.nil? ? NullAgent.new : agent}
+    ids.collect { |agent| agent.nil? ? NullAgent.new : agent }
   end
 end
